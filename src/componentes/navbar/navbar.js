fcbf3712fff6a36f84bc55b1ad38be92ad86c166
Milena Novka <103338752+MilenaNovka@users.noreply.github.com>
2026-08-06 16:24:40 -0300

    Navbar
    Botões do navbar arrumados

--- src/componentes/navbar/navbar.js
+++ src/componentes/navbar/navbar.js
@@ -3,7 +3,7 @@ function carregarNavbarHome(){
         <nav class="navbar nav-home">
             <div class="logo"><img src="/assets/imagens/PhilanceHome.png"> </div>
             <ul class "nav-links">
-                <button class="button-login" onclick="window.location.href='/src/pages/login/loginE&F.html'">Entrar</button>
+                <button class="btn btn-light" onclick="window.location.href='/src/pages/login/loginE&F.html'">Entrar</button>
                 <button class= "btn btn-dark" onclick="window.location.href='/src/pages/cadastro/cadastroE&F.html'">Cadastre-se</button>
             </ul>
         </nav>
